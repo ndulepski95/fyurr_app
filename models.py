fc4a2e9ae23c6a92ad90219db7c83ace9768a7ae
Nick Dulepski <ndulepski@gmail.com>
2020-12-15 20:48:27 -0500

    models key error

--- models.py
+++ models.py
@@ -1,12 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-import datetime
 
-db = SQLAlchemy()
+
+db=SQLAlchemy()
 
 class Venue(db.Model):
     __tablename__ = 'Venue'
+    __table_args__ = {'extend_existing': True}
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String())
@@ -20,10 +21,10 @@ class Venue(db.Model):
     facebook_link = db.Column(db.String(120))
     seeking_talent = db.Column(db.Boolean)
     seeking_description = db.Column(db.String(500))
-    
+    shows = db.relationship('Show', backref=db.backref('Venue'), lazy=True)  
 
     def __repr__(self):
-      return f'Venue id ={self.id},{self.name},{self.genres},{self.city},{self.state},{self.phone},{self.image_link},{self.shows},{self.facebook_link},{self.seeking_talent},{self.seeking_description},{self.shows}'
+      return f'<Venue {self.id} {self.name}>'
 
 class Artist(db.Model):
     __tablename__ = 'Artist'
@@ -40,19 +41,20 @@ class Artist(db.Model):
     facebook_link = db.Column(db.String(120))
     seeking_venue = db.Column(db.Boolean)
     seeking_description = db.Column(db.String(500))
+    shows = db.relationship('Show', backref=db.backref('Artist'), lazy=True)
     
-
     def __repr__(self):
-      return f'Artist id={self.id}, name={self.name},genres={self.genres},city={self.city},state={self.state},phone={self.phone},image_link={self.image_link},facekbook_link={self.facebook_link},seeking_venue={self.seeking_venue},seeking_description={self.seeking_description},shows={self.shows}>'
+      return f'<Artist {self.id} {self.name}'
+
+    
 
 class Show(db.Model):
     __tablename__ = 'Show'
     id = db.Column(db.Integer, primary_key=True)
-    artist_id = db.Column(db.Integer, db.ForeignKey('artist.id', onupdate='CASCADE', ondelete='CASCADE'), nullable=False)
-    venue_id = db.Column(db.Integer, db.ForeignKey('venue.id', onupdate='CASCADE', ondelete='CASCADE'), nullable=False)
+    artist_id = db.Column(db.Integer, db.ForeignKey('artist.id'), nullable=False)
+    venue_id = db.Column(db.Integer, db.ForeignKey('venue.id'), nullable=False)    
     start_time = db.Column(db.DateTime, nullable=False)
-    venue = db.relationship('Venue', backref=db.backref('shows', lazy=True))
-    artist = db.relationship('Artist', backref=db.backref('shows', lazy=True))
-
+    
     def __repr__(self):
-        return f'<Show id={self.id}, artist_id={self.artist_id},venue_id={self.venue_id}>'
+      return f'<Show {self.id}, Artist{self.artist_id}, Venue{self.venue_id}>'
+    
